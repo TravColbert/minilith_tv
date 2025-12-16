30324c89c73bfc4122f595f1740c2f71a6f8ca83
Trav Colbert <trav.colbert@gmail.com>
2025-12-16 07:15:47 -0800

    fix library path typo in options configuration

--- config/options.js
+++ config/options.js
@@ -3,7 +3,7 @@
 module.exports = function (app, appInstance) {
   return {
     libraryPaths: [
-      "path/to/you/media/library1",
+      "/path/to/your/media/library1",
     ],
     mediaExtensions: [".mp4", ".mkv", ".avi", ".mp3", ".flac"],
     vlcUrl: "http://localhost:8081/requests/status.xml",
